ae8e17298c8daf31c4ab77e8999870b89ba505d7
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-11-12 15:54:15 +0530

    imput data card completed

--- lib/widgets/input_data_card.dart
+++ lib/widgets/input_data_card.dart
@@ -8,6 +8,17 @@ class InputDataCard extends StatefulWidget {
 }
 
 class _InputDataCardState extends State<InputDataCard> {
+  // text editing controllers
+  TextEditingController _titleEditingController = TextEditingController();
+  TextEditingController _contentEditingController = TextEditingController();
+  // function to print data
+  void printdata() {
+    print(_titleEditingController.text);
+    print(_contentEditingController.text);
+    _titleEditingController.clear();
+    _contentEditingController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,14 +31,49 @@ class _InputDataCardState extends State<InputDataCard> {
         child: Column(
           children: [
             TextField(
+              controller: _titleEditingController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.black87),
                 ),
                 labelText: "Title",
                 hintText: "Enter the tile: ",
-                hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                hintStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
               ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: _contentEditingController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black87),
+                ),
+                labelText: "Content",
+                hintText: "Enter the Content: ",
+                hintStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 15),
+            // ElevatedButton(onPressed: printdata, child: Text("Print Data")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: printdata,
+                  child: Text(
+                    "Saved Data",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: 15),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Cancel", style: TextStyle(color: Colors.red)),
+                ),
+              ],
             ),
           ],
         ),
