8397c1a6123c08abccb45726b85ecb7fd0c478e6
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2026-01-19 18:21:36 +0530

    feat: enhance homepage to display journal entries with loading and error states

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:journal_app/data/data.dart';
 import 'package:journal_app/provider/auth_provider.dart';
+import 'package:journal_app/provider/journal_entry_provider.dart';
 import 'package:journal_app/screen/login_page.dart';
 import 'package:journal_app/widgets/general_entry_card.dart';
 import 'package:journal_app/widgets/input_data_card.dart';
@@ -27,6 +28,7 @@ class _HomepageState extends ConsumerState<Homepage> {
 
   @override
   Widget build(BuildContext context) {
+    final entriesValue = ref.watch(journalEntryProvider);
     return Scaffold(
       backgroundColor: Colors.black87,
       appBar: AppBar(
@@ -43,11 +45,59 @@ class _HomepageState extends ConsumerState<Homepage> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: listOfEntry.length,
-        itemBuilder: (context, index) {
-          return JournalEntryCard(entry: listOfEntry[index]);
+      body: entriesValue.when(
+        data: (entires) {
+          if (entires.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.book_outlined, size: 64, color: Colors.grey),
+                  const SizedBox(height: 16),
+                  Text(
+                    'No journal entries yet',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Tap the + button to add first entry',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodyMedium?.copyWith(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: listOfEntry.length,
+            itemBuilder: (context, index) {
+              return JournalEntryCard(entry: listOfEntry[index]);
+            },
+          );
         },
+        error: (error, stackTrace) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.error_outline, size: 64, color: Colors.red),
+                const SizedBox(height: 16),
+                Text(
+                  'Error loading entries',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  error.toString(),
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -59,10 +109,22 @@ class _HomepageState extends ConsumerState<Homepage> {
             ),
             builder: (context) {
               return InputDataCard(
-                onSave: (entry) {
-                  setState(() {
-                    listOfEntry.add(entry);
-                  });
+                onSave: (entry) async {
+                  try {
+                    final service = ref.read(journalEntryServiceProvider);
+                    await service.addEntry(entry);
+                    Navigator.pop(context);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Failed to save Entry: $e',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
                 },
               );
             },
